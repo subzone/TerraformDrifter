c61fc9417752281ef8eb6bb8907aeb34565ff167
subzone <subzone@live.com>
2024-03-22 09:14:41 +0100

    test

--- terraform/config.tf
+++ terraform/config.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "career-base-weu-dev"
     storage_account_name = "careerbaseweudev"
     container_name       = "tfstate"
-    key                  = "example.tfstate"
+    key                  = "test.tfstate"
   }
 }
