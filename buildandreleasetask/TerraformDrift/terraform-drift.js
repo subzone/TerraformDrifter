eb73a2d8aa6fb32966e0df93ae588775dd9c7b76
subzone <subzone@live.com>
2024-03-22 15:04:15 +0100

    add support for diff providers

--- buildandreleasetask/TerraformDrift/terraform-drift.js
+++ buildandreleasetask/TerraformDrift/terraform-drift.js
@@ -1,20 +1,58 @@
 const { spawnSync } = require('child_process');
 const tl = require('azure-pipelines-task-lib/task');
 
+let provider = tl.getInput('provider', true);
+
+switch (provider) {
+  case 'azure':
+    let azureSubscription = tl.getInput('azureSubscription', true);
+    let servicePrincipalId = tl.getEndpointAuthorizationParameter(azureSubscription, 'serviceprincipalid', false);
+    let servicePrincipalKey = tl.getEndpointAuthorizationParameter(azureSubscription, 'serviceprincipalkey', false);
+    let subscriptionId = tl.getEndpointDataParameter(connectedService, 'subscriptionid', true);
+    let tenantId = tl.getEndpointAuthorizationParameter(azureSubscription, 'tenantid', false);
+
+    process.env['ARM_TENANT_ID'] = tenantId;
+    process.env['ARM_SUBSCRIPTION_ID'] = subscriptionId;
+    process.env['ARM_CLIENT_ID'] = servicePrincipalId;
+    process.env['ARM_CLIENT_SECRET'] = servicePrincipalKey;
+    break;
+
+  case 'aws':
+    let awsSubscription = tl.getInput('awsSubscription', true);
+    let accessKeyId = tl.getEndpointAuthorizationParameter(awsSubscription, 'accesskeyid', false);
+    let secretAccessKey = tl.getEndpointAuthorizationParameter(awsSubscription, 'secretaccesskey', false);
+
+    process.env['AWS_ACCESS_KEY_ID'] = accessKeyId;
+    process.env['AWS_SECRET_ACCESS_KEY'] = secretAccessKey;
+    break;
+
+  case 'gcp':
+    let gcpSubscription = tl.getInput('gcpSubscription', true);
+    let keyFile = tl.getEndpointAuthorizationParameter(gcpSubscription, 'keyfile', false);
+
+    process.env['GOOGLE_APPLICATION_CREDENTIALS'] = keyFile;
+    break;
+
+  default:
+    console.error('Invalid provider');
+    process.exit(1);
+}
+
+
 let workingDirectory = tl.getInput('workingDirectory', true);
-let connectedService = tl.getInput('azureSubscription', true);
-let tenantId = tl.getEndpointAuthorizationParameter(connectedService, 'tenantid', false);
-let servicePrincipalId = tl.getEndpointAuthorizationParameter(connectedService, 'serviceprincipalid', false);
-let servicePrincipalKey = tl.getEndpointAuthorizationParameter(connectedService, 'serviceprincipalkey', false);
-let subscriptionId = tl.getEndpointDataParameter(connectedService, 'subscriptionid', true);
+// let connectedService = tl.getInput('azureSubscription', true);
+// let tenantId = tl.getEndpointAuthorizationParameter(connectedService, 'tenantid', false);
+// let servicePrincipalId = tl.getEndpointAuthorizationParameter(connectedService, 'serviceprincipalid', false);
+// let servicePrincipalKey = tl.getEndpointAuthorizationParameter(connectedService, 'serviceprincipalkey', false);
+// let subscriptionId = tl.getEndpointDataParameter(connectedService, 'subscriptionid', true);
 
 console.log('Tenant ID: ', tenantId);
 // console.log('Service Principal ID: ', servicePrincipalId);
 // console.log('Service Principal Key: ', servicePrincipalKey);
-process.env['ARM_TENANT_ID'] = tenantId;
-process.env['ARM_SUBSCRIPTION_ID'] = subscriptionId;
-process.env['ARM_CLIENT_ID'] = servicePrincipalId;
-process.env['ARM_CLIENT_SECRET'] = servicePrincipalKey;
+// process.env['ARM_TENANT_ID'] = tenantId;
+// process.env['ARM_SUBSCRIPTION_ID'] = subscriptionId;
+// process.env['ARM_CLIENT_ID'] = servicePrincipalId;
+// process.env['ARM_CLIENT_SECRET'] = servicePrincipalKey;
 
 process.on('uncaughtException', function(err) {
   console.error('Caught exception: ', err);
