bb424b19930306c4a5d9cc754edc1bc76a2d3018
subzone <subzone@live.com>
2024-03-26 22:34:04 +0100

    try exec

--- buildandreleasetask/TerraformDrift/operations/handleTofuOperations.js
+++ buildandreleasetask/TerraformDrift/operations/handleTofuOperations.js
@@ -1,102 +1,62 @@
-const { spawnSync } = require('child_process');
+const { exec } = require('child_process');
 const tl = require('azure-pipelines-task-lib/task');
 const fs = require('fs');
-
-// let autoReconcile = true; // or false depending on your needs
-const autoReconcile = tl.getBoolInput('autoReconcile', false);
 const path = require('path');
 
-/**
- * Handles tofu operations for a given working directory.
- *
- * @param {string} workingDirectory - The working
- *  directory for the tofu operations.
- */
+const autoReconcile = tl.getBoolInput('autoReconcile', false);
+
 function handleTofuOperations(workingDirectory) {
     console.log('Working directory: ', workingDirectory);
     console.log('Files in working directory: ', fs.readdirSync(workingDirectory));
     const absoluteWorkingDirectory = path.resolve(workingDirectory).trim();
     console.log('Absolute working directory: ', absoluteWorkingDirectory);
-  
-    // Initialize tofu
-    const initArgs = [
-      `-e ARM_CLIENT_ID=${process.env.ARM_CLIENT_ID}`,
-      `-e ARM_CLIENT_SECRET=${process.env.ARM_CLIENT_SECRET}`,
-      `-e ARM_SUBSCRIPTION_ID=${process.env.ARM_SUBSCRIPTION_ID}`,
-      `-e ARM_TENANT_ID=${process.env.ARM_TENANT_ID}`,
-      `--workdir=/app`,
-      `-v ${absoluteWorkingDirectory.trim()}/:/app`,
-      'ghcr.io/subzone/opentofu:latest'
-    ];
-    console.log('Docker command: docker','run', ...initArgs, 'init');
-    const init = spawnSync('docker',['run', ...initArgs, 'init'], { stdio: 'inherit' });
-    console.log('Init command=', init);
-    if (init.stdout) {
-      console.log(init.stdout.toString());
-    } else {
-      console.error('Docker command failed with error: ', init.error);
-    }
-    if (init.error) {
-      console.error('\x1b[31m%s\x1b[0m', 'Error: tofu init failed');
-      process.exit(1);
-    }
-  
-    // Check for drift
-    const dockerArgs = [
-      `-e ARM_CLIENT_ID=${process.env.ARM_CLIENT_ID}`,
-      `-e ARM_CLIENT_SECRET=${process.env.ARM_CLIENT_SECRET}`,
-      `-e ARM_SUBSCRIPTION_ID=${process.env.ARM_SUBSCRIPTION_ID}`,
-      `-e ARM_TENANT_ID=${process.env.ARM_TENANT_ID}`,
-      `--workdir=/app`,
-      `-v ${absoluteWorkingDirectory.trim()}/:/app`,
-      'ghcr.io/subzone/opentofu:latest'
-    ];
-    console.log('Docker command: docker', ...dockerArgs)
-    const plan = spawnSync('docker',[ 'run', ...dockerArgs, 'plan', '-detailed-exitcode' ], { stdio: 'inherit' });
-     if (plan.stdout) {
-      console.log(plan.stdout.toString());
-    } else {
-      console.error('Docker command failed with error: ', plan.error);
-    }
-    if (plan.error) {
-      console.error('\x1b[31m%s\x1b[0m', 'Error: tofu plan failed');
-      process.exit(1);
-    }
-    // If the exit code is 2, there is drift
-    if (plan.status === 2) {
-      if (autoReconcile) {
-        console.log('\x1b[33m%s\x1b[0m\n\x1b[33m%s\x1b[0m',
-            'Drift detected.',
-            ' AutoReconciliation parameter set to true.',
-            'Reconciling...');
-        const applyArgs = [
-          `-e ARM_CLIENT_ID=${process.env.ARM_CLIENT_ID}`,
-          `-e ARM_CLIENT_SECRET=${process.env.ARM_CLIENT_SECRET}`,
-          `-e ARM_SUBSCRIPTION_ID=${process.env.ARM_SUBSCRIPTION_ID}`,
-          `-e ARM_TENANT_ID=${process.env.ARM_TENANT_ID}`,
-          `--workdir=/app`,
-          `-v ${absoluteWorkingDirectory.trim()}/:/app`,
-          'ghcr.io/subzone/opentofu:latest' 
-        ];
-        console.log('Docker command: docker', 'run', ...applyArgs, 'apply', '-auto-approve');
-        const apply = spawnSync('docker', ['run', ...applyArgs, 'apply', '-auto-approve' ], { cwd: workingDirectory, stdio: 'inherit' });
-        if (apply.stdout) {
-          console.log(apply.stdout.toString());
-        } else {
-          console.error('Docker command failed with error: ', apply.error);
+
+    const dockerCommand = `docker run -e ARM_CLIENT_ID=${process.env.ARM_CLIENT_ID} -e ARM_CLIENT_SECRET=${process.env.ARM_CLIENT_SECRET} -e ARM_SUBSCRIPTION_ID=${process.env.ARM_SUBSCRIPTION_ID} -e ARM_TENANT_ID=${process.env.ARM_TENANT_ID} --workdir=/app -v ${absoluteWorkingDirectory.trim()}:/app ghcr.io/subzone/opentofu:latest`;
+
+    exec(`${dockerCommand} init`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error: ${error.message}`);
+            return;
         }
-        if (apply.error) {
-          console.error('Error: tofu apply failed');
-          process.exit(1);
+        if (stderr) {
+            console.error(`Stderr: ${stderr}`);
+            return;
         }
-      } else {
-        console.log('\x1b[32m%s\x1b[0m',
-            'Auto Reconciliation is set to false, please reconcile manually.');
-      }
-    } else {
-      console.log('\x1b[32m%s\x1b[0m',
-          'No drift detected.');
-    }
-  }
-  
-  module.exports = handleTofuOperations;
+        console.log(`Stdout: ${stdout}`);
+
+        exec(`${dockerCommand} plan -detailed-exitcode`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.error(`Stderr: ${stderr}`);
+                return;
+            }
+            console.log(`Stdout: ${stdout}`);
+
+            if (stdout.includes('exit code 2')) {
+                if (autoReconcile) {
+                    console.log('Drift detected. AutoReconciliation parameter set to true. Reconciling...');
+                    exec(`${dockerCommand} apply -auto-approve`, (error, stdout, stderr) => {
+                        if (error) {
+                            console.error(`Error: ${error.message}`);
+                            return;
+                        }
+                        if (stderr) {
+                            console.error(`Stderr: ${stderr}`);
+                            return;
+                        }
+                        console.log(`Stdout: ${stdout}`);
+                    });
+                } else {
+                    console.log('Auto Reconciliation is set to false, please reconcile manually.');
+                }
+            } else {
+                console.log('No drift detected.');
+            }
+        });
+    });
+}
+
+module.exports = handleTofuOperations;
